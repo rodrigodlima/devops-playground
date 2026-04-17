0ebe1a1b34168405406de5b3d79a484f32109698
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-04-17 11:48:56 -0300

    Add Terraform provider configuration for AWS with required settings

---
+++ DevSecOps/DependencyTrack/opentofu/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.41.0"
+    }
+  }
+}
+
+provider "aws" {
+    shared_config_files      = ["/Users/rodrigo/.aws/config"]
+    shared_credentials_files = ["/Users/rodrigo/.aws/credentials"]
+    profile                  = "customprofile"
+}
